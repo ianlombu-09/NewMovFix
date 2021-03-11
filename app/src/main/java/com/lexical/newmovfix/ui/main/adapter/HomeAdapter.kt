c534c4bb4b9fb398c083cbd67be57ae92c765e69
ian lombu <citrakemurnian@gmail.com>
2021-03-12 01:15:23 +0700

    Adapter; Activity; layout

--- app/src/main/java/com/lexical/newmovfix/ui/main/adapter/HomeAdapter.kt
+++ app/src/main/java/com/lexical/newmovfix/ui/main/adapter/HomeAdapter.kt
@@ -1,5 +1,41 @@
 package com.lexical.newmovfix.ui.main.adapter
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.lexical.newmovfix.R
+import com.lexical.newmovfix.data.model.MovieModel
+import com.lexical.newmovfix.data.model.MoviePopularResponse
+import kotlinx.android.synthetic.main.recycler_popular_movie.view.*
 
+class HomeAdapter(
+        private val movies: ArrayList<MovieModel>
+        ): RecyclerView.Adapter<HomeAdapter.DataViewHolder>() {
+
+    class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(movies: MovieModel) {
+            itemView.movie_title.text = movies.title
+            Glide.with(itemView.movie_img.context)
+                    .load(movies.poster_path)
+                    .into(itemView.movie_img)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            DataViewHolder (
+                LayoutInflater.from(parent.context).inflate(
+                        R.layout.recycler_popular_movie, parent, false
+                )
+            )
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: DataViewHolder, position: Int) =
+            holder.bind(movies[position])
+
+    fun addData(list: List<MovieModel>) {
+        movies.addAll(list)
+    }
 }
--- app/src/main/java/com/lexical/newmovfix/ui/main/view/HomeActivity.kt
+++ app/src/main/java/com/lexical/newmovfix/ui/main/view/HomeActivity.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.lexical.newmovfix.R
+import com.lexical.newmovfix.data.model.MovieModel
 import com.lexical.newmovfix.ui.main.adapter.HomeAdapter
 import com.lexical.newmovfix.ui.main.viewmodel.HomeViewModel
 import com.lexical.newmovfix.utils.Status
@@ -22,9 +25,20 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        setupUI()
         setUpObserverPopular()
+    }
 
+    private fun setupUI() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = HomeAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+                DividerItemDecoration(
+                        recyclerView.context,
+                        (recyclerView.layoutManager as LinearLayoutManager).orientation
+                )
+        )
+        recyclerView.adapter = adapter
     }
 
     private fun setUpObserver() {
@@ -48,10 +62,13 @@ class HomeActivity : AppCompatActivity() {
         homeViewModel.popularMovies.observe(this, Observer {
             when (it.status) {
                 Status.SUCCESS -> {
-                    Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                    progressBar.visibility = View.GONE
+                    //it.data?.let { movies -> renderList(movies) }
+                    recyclerView.visibility = View.VISIBLE
                 }
                 Status.LOADING -> {
-                    Toast.makeText(this, "Loading", Toast.LENGTH_SHORT).show()
+                    progressBar.visibility = View.VISIBLE
+                    recyclerView.visibility = View.GONE
                 }
                 Status.ERROR -> {
                     progressBar.visibility = View.GONE
@@ -59,5 +76,10 @@ class HomeActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    private fun renderList(movies: List<MovieModel>) {
+        adapter.addData(movies)
+        adapter.notifyDataSetChanged()
     }
 }
